dd4c73e3881dcb6e608252a712cbaefc18a8f9a5
Martin Gurtner <martin.gurtner@fel.cvut.cz>
2020-07-20 11:37:20 +0200

    Plotting module added

--- src/DistCtrl4DistMan.jl
+++ src/DistCtrl4DistMan.jl
@@ -10,13 +10,217 @@ include("ActuatorArrayModule.jl")
 include("ObjectAgentModule.jl")
 include("ConvAnalysisModule.jl")
 include("ADMMModule.jl")
+include("PlottingModule.jl")
 
+using LinearAlgebra
+using Plots
+using Printf
 
 using .LinSolvers
 using .ActuatorArrayModule
 using .ObjectAgentModule
 using .ADMMModule
+using .ConvAnalysisModule
+using .PlottingModule
 
-# Write your package code here.
+function genRandomPositions(N, params, xlim, ylim, minMutualDist)
+    @assert (params["space_dim"] == 2 || params["space_dim"] == 3) "Unsupported dimension."
+
+    F = typeof(minMutualDist);
+    oa_pos = NTuple{params["space_dim"], F}[];
+
+    N_attempts = 500;
+    for l in 1:N_attempts
+        # Generate a random position for kth agent
+        xrand = (xlim[2] - xlim[1])*rand(F) + xlim[1];
+        yrand = (ylim[2] - ylim[1])*rand(F) + ylim[1];
+        if params["space_dim"] == 2
+            oa_posk = ( xrand, yrand );
+        else params["space_dim"] == 3
+            oa_posk = ( xrand, yrand, params["z0"]);
+        end
+
+        # oa_pos = ((n-1)*aa.dx/2, (n-1)*aa.dx/2, z0);
+        # Check whether outher poitns are far enough from the kth point
+        theyAreFarEnough = true;
+        for pos in oa_pos
+            if norm(pos .- oa_posk) < minMutualDist
+                theyAreFarEnough = false;
+                continue;
+            end
+        end
+        # if they are far enough, break from the loop
+        if theyAreFarEnough
+            push!(oa_pos, oa_posk);
+
+            length(oa_pos) == N && break
+        end
+    end
+
+    if length(oa_pos) != N
+        error("Failed to generate random positions of the agents.")
+    end
+
+    oa_pos;
+end
+
+function initAgents(aa::ActuatorArray{T, U}, oa_pos::Union{Array{Tuple{T,T},1}, Array{Tuple{T,T,T}, 1}}, params::Dict{String,Any}; Fdes = missing) where {T<:Real, U<:Unsigned}
+    N = size(oa_pos)[1];
+
+    if params["platform"] == :DEP
+        agents = Array{ObjectAgent_DEP{T, U}, 1}();
+        rand_controls = 2*π*rand(T, aa.nx, aa.ny);
+        for k in 1:N
+            # Generate list of used actuators for each object agent
+            aL, a_used = genActList(aa, oa_pos[k], params["maxDist"][1], params["maxDist"][2]);
+            if ismissing(Fdes)
+                Fdes_i = calcDEPForce(aa, oa_pos[k], rand_controls).*(1/2,1/2,1/2);
+            else
+                Fdes_i = Fdes[k];
+            end
+            push!(agents, ObjectAgent_DEP("Agent $k", oa_pos[k], (Fdes_i[1], Fdes_i[2], Fdes_i[3]), aa, aL, a_used));
+        end
+    elseif params["platform"] == :MAG
+        agents = Array{ObjectAgent_MAG{T, U}, 1}();
+        rand_controls = rand(T, aa.nx, aa.ny);
+        for k in 1:N
+            # Generate list of used actuators for each object agent
+            aL, a_used = genActList(aa, (oa_pos[k][1], oa_pos[k][2], T(0)), params["maxDist"][1], params["maxDist"][2]);
+            if ismissing(Fdes)
+                Fdes_i = calcMAGForce(aa, oa_pos[k], rand_controls)./2;
+            else
+                Fdes_i = Fdes[k];
+            end
+            push!(agents, ObjectAgent_MAG("Agent $k", oa_pos[k], (Fdes_i[1], Fdes_i[2]), aa, aL, a_used, params["λ"]));
+        end
+    elseif params["platform"] == :ACU
+        agents = Array{ObjectAgent_ACU{T, U}, 1}();
+        for k in 1:N
+            # Generate list of used actuators for each object agent
+            aL, a_used = genActList(aa, oa_pos[k], params["maxDist"][1], params["maxDist"][2]);
+
+            push!(agents, ObjectAgent_ACU("Agent $k", oa_pos[k], params["Pdes"], aa, aL, a_used));
+        end
+    else
+        error("Unsupported platform")
+    end
+
+    return agents;
+end
+
+function collapseControls(agents, aa)
+    # Averages the controls over the agents
+
+    controls = fill(NaN, aa.nx, aa.ny);
+    actuatorIsUsedByN = zeros(size(controls))
+    for agent in agents
+        for (k, (i, j)) in enumerate(agent.actList)
+            actuatorIsUsedByN[i,j] += 1;
+            if isnan(controls[i,j])
+                controls[i,j] = agent.zk[k];
+            else
+                controls[i,j] += 1/actuatorIsUsedByN[i,j] * (agent.zk[k] - controls[i,j]);
+            end
+        end
+    end
+
+    return controls;
+end
+
+function print_err_stats(exp_data, params)
+        # Print some output saying how good is the result of the distributed optimization
+        println("Req.  \t\t\t | Dev.")
+        for (k, agnt) in enumerate(exp_data["Agents"])
+            if exp_data["Platform"] == :ACU
+                printf("%4.1f \t\t\t%4.1f\n", agnt.Pdes, params["calcForce"](exp_data["ActuatorArray"], agnt.pos, exp_data["Controls"]))
+            else
+                force_dim = length(agnt.Fdes);
+                if force_dim > 1
+                    Fdev = params["calcForce"](exp_data["ActuatorArray"], agnt.pos, exp_data["Controls"]) ./ agnt.Fdes_sc;
+                    printf("[" * "% 1.3f, "^(force_dim-1) *"% 1.3f]\t [" * "% 1.3f, "^(force_dim-1) *"% 1.3f]\n", agnt.Fdes..., Fdev...)
+                else
+                    error("Unsupported platform")
+                end
+            end
+        end
+        # Value of the cost function in the last iteration
+        println("\nFinal-itreation error")
+        # println([exp_data["conv_data"][end][i].cost for i in 1:size(exp_data)[3]])
+        println([agent.cost for agent in exp_data["conv_data"][end]])
+end
+
+
+function showPlots(exp_data, params::Dict{String,Any}, f_convAnalysis; f_plotControls=false, f_showCircles=false)
+    if f_showCircles
+        distCircles = params["maxDist"];
+    else
+        distCircles = missing;
+    end
+
+    ## Plot the actuator array together with the objects
+    if exp_data["Platform"] == :ACU
+        if !f_convAnalysis
+            Plots.display(plot(plot(exp_data["ActuatorArray"], agents=exp_data["Agents"], legend=nothing, distCircles=distCircles),
+            heatmap(exp_data["ActuatorArray"], params["z0"], exp_data["Controls"], agents=exp_data["Agents"], N=100, box=:grid, colorbar=false),
+            layout=@layout [a b]))
+        else
+            Plots.display(plot(plot(exp_data["ActuatorArray"], agents=exp_data["Agents"], legend=nothing, distCircles=distCircles),
+            heatmap(exp_data["ActuatorArray"], params["z0"], exp_data["Controls"], agents=exp_data["Agents"], N=100, box=:grid, colorbar=false),
+            plot(errf(exp_data["conv_data"]), leg=false, yticks=0:0.5:1, border=:origin, title="Error convergence", titlefontsize=8),
+            plot(conv_measure(exp_data["conv_data"]), leg=false, title="Error convergence", titlefontsize=8, yscale=:log10), size=(500,500), layout=@layout [[a b]; c{0.1h}; d{0.1h}]))
+        end
+    else
+        Fdev_arr = Array{NTuple{params["force_dim"], Float64}, 1}();
+        for agnt in exp_data["Agents"]
+            Fdev = params["calcForce"](exp_data["ActuatorArray"], agnt.pos, exp_data["Controls"]);
+            push!(Fdev_arr, Fdev);
+        end
+
+        if f_plotControls
+            controls = exp_data["Controls"];
+        else
+            controls = fill(NaN, exp_data["ActuatorArray"].nx, exp_data["ActuatorArray"].ny);
+        end
+
+        if !f_convAnalysis
+            Plots.display(plot(exp_data["ActuatorArray"], controls = controls, agents=exp_data["Agents"], dev_forces=Fdev_arr, showReqForces=true, Fscale=params["Fscale"], legend=nothing, size=(500,500), distCircles=distCircles))
+        else
+            Plots.display(plot(plot(exp_data["ActuatorArray"], controls = controls, agents=exp_data["Agents"], dev_forces=Fdev_arr, showReqForces=true, Fscale=params["Fscale"], legend=nothing, distCircles=distCircles),
+            plot(errf(exp_data["conv_data"]), leg=false, yticks=0:0.5:1, border=:origin, title="Error convergence", titlefontsize=8),
+            plot(conv_measure(exp_data["conv_data"]), leg=false, title="Error convergence", titlefontsize=8, yscale=:log10),  size=(500,600), layout=@layout [a; b{0.1h};c{0.1h}]))
+        end
+    end
+end
+
+function savePlots(exp_data, params::Dict{String,Any}; f_plotControls=false, filename="TestArray.pdf", f_showCircles=false)
+    if f_showCircles
+        distCircles = params["maxDist"];
+    else
+        distCircles = missing;
+    end
+
+    if exp_data["Platform"] == :ACU
+        plot(plot(exp_data["ActuatorArray"], agents=exp_data["Agents"], legend=nothing, distCircles=distCircles),
+        heatmap(exp_data["ActuatorArray"], params["z0"], exp_data["Controls"], agents=exp_data["Agents"], N=120, box=:grid, colorbar=false),
+        size=(1000,500), layout=@layout [a b])
+        annotate!([(agnt.pos[1]+exp_data["ActuatorArray"].dx/6, agnt.pos[2]+exp_data["ActuatorArray"].dx/6, text(string(round(calcPressure(exp_data["ActuatorArray"], agnt.pos, exp_data["Controls"]))), 12, :white, :left, :bottom, :bold)) for agnt in exp_data["Agents"]], subplot=2)
+        savefig(string("figs/", filename))
+    else
+        Fdev_arr = Array{NTuple{params["force_dim"], Float64}, 1}();
+        for agnt in exp_data["Agents"]
+            Fdev = params["calcForce"](exp_data["ActuatorArray"], agnt.pos, exp_data["Controls"]);
+            push!(Fdev_arr, Fdev);
+        end
+
+        if f_plotControls
+            controls = exp_data["Controls"];
+        else
+            controls = fill(NaN, exp_data["ActuatorArray"].nx, exp_data["ActuatorArray"].ny);
+        end
+
+        plot(exp_data["ActuatorArray"], controls=controls, agents=exp_data["Agents"], dev_forces=Fdev_arr, showReqForces=true, Fscale=params["Fscale"], legend=nothing, size=(500,500), lwidth=8, distCircles=distCircles)
+        savefig(string("figs/", filename))
+    end
+end
 
 end
---
+++ src/PlottingModule.jl
@@ -0,0 +1,357 @@
+module PlottingModule
+
+using Plots
+using Printf
+
+using ..ObjectAgentModule
+using ..ActuatorArrayModule
+
+export plot_AA, visu_pressure
+
+# Plotting function
+@recipe function plot_AA(aa::ActuatorArray{T,U};
+    controls = fill(NaN, aa.nx, aa.ny),
+    ctrlim = (0, 1),
+    controls_clr_fun = α -> RGBA(1, 1-α, 1-α, 1),
+    boxes = Array{Array{Tuple{U,U},1},1}(),
+    agents = Array{ObjectAgent, 1}(),
+    agents_clrs = missing,
+    dev_forces = Array{Tuple{T,T,T}, 1}(),
+    aux_forces = Array{Tuple{T,T,T}, 1}(),
+    ref_pos = Array{Tuple{T,T,T}, 1}(),
+    distCircles = missing,
+    Fscale = 1,
+    plotUsedActuators = true,
+    plotUsedActuators_enable = missing,
+    showAgentNames = true,
+    showReqForces = false,
+    lwidth = 8,
+    Fdev_clr = :yellow,
+    Faux_clr = :green,
+    Freq_clr = :red)  where {T<:Real,U<:Unsigned}
+    # if controls is provided, check whether it has the correct size
+    @assert size(controls) == (aa.nx, aa.ny) "The matrix of phase-shifts doesn't have the correct size!"
+
+    if plotUsedActuators && length(agents) > 0 && ismissing(plotUsedActuators_enable)
+        plotUsedActuators_enable = fill(true, length(agents));
+    end
+
+    aspect_ratio := :equal
+    framestyle := :border
+
+    if aa.dx < 1e-3
+        scale = 1e3;
+    elseif aa.dx < 1
+        scale = 1e1;
+    else
+        scale = 1;
+    end
+
+    xlims := (-aa.dx/2, (aa.nx-0.5)*aa.dx)
+    ylims := (-aa.dx/2, (aa.ny-0.5)*aa.dx)
+
+    ticks := nothing
+
+    # Generate boxes for plotting the used actuators
+    x_box = [-aa.dx/2, aa.dx/2, aa.dx/2, -aa.dx/2];
+    y_box = [-aa.dx/2, -aa.dx/2, aa.dx/2, aa.dx/2];
+
+    # if boxes are provided plot the boxes around the actuators
+    if length(boxes) != 0
+        box_clrs = distinguishable_colors(length(boxes), lchoices = range(40, stop=80, length=15), cchoices = range(30, stop=100, length=15))
+        k = 1;
+        for k in eachindex(boxes)
+            for act_box in boxes[k]
+                @series begin
+                    xc, yc = actuatorPosition(aa, act_box)
+
+                    fill  := (0, box_clrs[k]);
+                    label := "";
+                    linewidth := 0;
+
+                    x_box .+ xc, y_box .+ yc
+                end
+            end
+        end
+    end
+
+    if length(agents) != 0
+        if ismissing(agents_clrs)
+            agents_clrs = distinguishable_colors(length(agents), lchoices = range(40, stop=80, length=15), cchoices = range(30, stop=80, length=15));
+        end
+    end
+
+    # If a list of agents is provided, plot boxes around actuators used by the agents
+    if length(agents) != 0 && plotUsedActuators
+        for (k, agent) in enumerate(agents)
+            if !plotUsedActuators_enable[k]
+                continue;
+            end
+
+            if ismissing(distCircles)
+                for (ak, act_box) in enumerate(agent.actList)
+                    if ak ∉ agent.act_used
+                        clralpha = 0.25;
+                    else
+                        clralpha = 0.5;
+                    end
+                    @series begin
+                        xc, yc = actuatorPosition(aa, act_box)
+
+                        fill  := (0, clralpha, agents_clrs[k])
+                        label := ""
+                        linealpha := 0
+
+                        x_box .+ xc, y_box .+ yc
+                    end
+                end
+            else
+                th = range(0, 2*π, length=25);
+                xc, yc = agent.pos;
+                @series begin
+                    fill  := (0, 0.5, agents_clrs[k])
+                    seriescolor := agents_clrs[k]
+                    label := ""
+                    linealpha := 1
+
+                    sin.(th).*distCircles[1] .+ xc, cos.(th).*distCircles[1] .+ yc
+                end
+                @series begin
+                    fill  := (0, 0.25, agents_clrs[k])
+                    seriescolor := agents_clrs[k]
+                    label := ""
+                    linealpha := 1
+
+                    sin.(th).*distCircles[2] .+ xc, cos.(th).*distCircles[2] .+ yc
+                end
+            end
+        end
+    end
+
+    # Generate circles for plotting the actuators
+    if aa.shape == :circle || aa.shape == :coil
+        th = range(0, 2*π, length=25);
+        x_act_box = aa.width*cos.(th)./2;
+        y_act_box = aa.width*sin.(th)./2;
+    elseif aa.shape == :square
+        x_act_box = [-aa.width/2, aa.width/2, aa.width/2, -aa.width/2, -aa.width/2];
+        y_act_box = [-aa.width/2, -aa.width/2, aa.width/2, aa.width/2, -aa.width/2];
+    else
+        error("Unsupported shape of the actuators. The shape must be either :circle or :square")
+    end
+
+    # Iterate over indexes of the actuators
+    for (ai, aj) in aa
+        # get the position of the acutator with indexes (ai, aj)
+        ax, ay = actuatorPosition(aa, (ai, aj));
+
+        @series begin
+            seriescolor := :black
+            label := ""
+
+            if ~isnan(controls[ai,aj])
+                α = (controls[ai,aj] - ctrlim[1]) / (ctrlim[2] - ctrlim[1]);
+                # fill_clr = RGBA(0.4*α + 0.6, 0, 0.4*(1-α)+0.6, 1);
+                fill_clr = controls_clr_fun(α);
+            else
+                fill_clr = RGBA(1, 1, 1, .8);
+            end
+            # fill  := (0, fill_clr)
+            fill  := (0, fill_clr)
+
+            x_act_box .+ ax, y_act_box .+ ay
+        end
+
+        if aa.shape == :coil
+            @series begin
+                seriescolor := :black
+                label := ""
+
+                fill  := (0, RGBA(0.6, 0.6, 0.6, 1))
+
+                0.5.*x_act_box .+ ax, 0.5.*y_act_box .+ ay
+            end
+        end
+    end
+
+    # If a list of agents is provided, plot dots at the places of the agents
+    if length(agents) != 0
+        if !isa(agents[1], ObjectAgent_MAG) && !isa(agents[1], ObjectAgent_DEP)
+            for (k, agent) in enumerate(agents)
+                @series begin
+                    markershape := :circle;
+                    markersize := 10;
+                    markercolor := agents_clrs[k];
+                    if showAgentNames
+                        label := agent.name;
+                    else
+                        label := "";
+                    end
+                    legendfontsize := 4;
+                    linealpha := 0;
+
+                    xc, yc = agent.pos;
+                    [xc], [yc]
+                end
+            end
+        else
+            if isa(agents[1], ObjectAgent_MAG)
+                rad = 10e-3;
+            else
+                rad = 25e-6;
+            end
+
+            th = range(0, 2*π, length=25);
+            x_agt_box = rad.*cos.(th);
+            y_agt_box = rad.*sin.(th);
+
+            for (k, agent) in enumerate(agents)
+                xc, yc = agent.pos;
+                @series begin
+                    seriescolor := :black
+                    fill  := (0, agents_clrs[k]);
+                    if showAgentNames
+                        label := agent.name;
+                    else
+                        label := "";
+                    end
+                    legendfontsize := 4;
+
+                    xc .+ x_agt_box, yc .+ y_agt_box
+                end
+            end
+        end
+
+    end
+
+    # If agents and forces are provided, visualize force at the locations of the agents (Works only for ObjectAgent_DEP)
+    if length(agents) != 0 && length(dev_forces) != 0 || showReqForces
+        for (k, agent) in enumerate(agents)
+            @assert (agent isa ObjectAgent_DEP) || (agent isa ObjectAgent_MAG) "Forces can be plotted only for ObjectAgent_DEP or ObjectAgent_MAG"
+
+            xc, yc = agent.pos;
+            # Developed force (provided in dev_forces[])
+            if length(dev_forces) != 0
+                @series begin
+                    linecolor := Fdev_clr;
+                    # linestyle := :dot;
+                    linewidth := lwidth;
+                    label := "";
+
+                    [xc, xc + Fscale*dev_forces[k][1]*aa.dx], [yc, yc + Fscale*dev_forces[k][2]*aa.dx]
+                end
+            end
+
+            # If available, plot also forces in  aux_forces[]
+            if length(aux_forces) != 0
+                @series begin
+                    linecolor := Faux_clr;
+                    # linestyle := :dot;
+                    linewidth := lwidth;
+                    label := "";
+
+                    [xc, xc + Fscale*aux_forces[k][1]*aa.dx], [yc, yc + Fscale*aux_forces[k][2]*aa.dx]
+                end
+            end
+
+            # Required force (stored by each agents in agent.Fdes)
+            if showReqForces
+                @series begin
+                    linecolor := Freq_clr;
+                    # linestyle := :dash;
+                    label := "";
+                    linealpha := 0.65;
+                    linewidth := 0.65*lwidth;
+
+                    [xc, xc + Fscale*agent.Fdes[1]*agent.Fdes_sc*aa.dx], [yc, yc + Fscale*agent.Fdes[2]*agent.Fdes_sc*aa.dx]
+                end
+            end
+
+            if length(agent.Fdes) == 3 && showReqForces && length(dev_forces) != 0
+                # Visualize relativr error in z-component of the developed force
+                Fz_relerr = abs((agent.Fdes[3]*agent.Fdes_sc - dev_forces[k][3])/agent.Fdes[3]);
+                @series begin
+                    markershape := :circle;
+                    markersize := 10*Fz_relerr;
+                    markercolor := :red;
+                    label := "";
+                    linewidth := 0;
+                    markerstrokewidth := 0;
+
+                    [xc], [yc]
+                end
+            end
+        end
+    end
+
+    # If reference positions are provided, plot these
+    if length(agents) != 0 && length(ref_pos) != 0
+        for (k, agent) in enumerate(agents)
+            @series begin
+                x_ref, y_ref = ref_pos[k];
+                markershape := :circle;
+                markersize := 10;
+                markercolor := agents_clrs[k];
+                markeralpha := 0.75;
+                markerstrokewidth := 4;
+                markerstrokealpha := 0.75;
+                markerstrokecolor := :white;
+                linewidth := 0;
+                label := "";
+
+                [x_ref], [y_ref]
+            end
+        end
+    end
+end
+
+
+@recipe function visu_pressure(aa::ActuatorArray{T,U},
+    z0::T,
+    phases::Array{T,2};
+    agents = Array{ObjectAgent, 1}(),
+    N = 75) where {T<:Real, U<:Unsigned}
+    @assert size(phases) == (aa.nx, aa.ny) "The matrix of phase-shifts doesn't have the correct size!"
+
+    aspect_ratio := :equal
+    framestyle := :border
+    xlims := (aa.dx/2, (aa.nx-0.5)*aa.dx)
+    ylims := (aa.dx/2, (aa.ny-0.5)*aa.dx)
+
+    ticks := nothing
+
+    xv = LinRange(-aa.dx/2,aa.nx*aa.dx-aa.dx/2, N);
+    yv = LinRange(-aa.dx/2,aa.nx*aa.dx-aa.dx/2, N);
+    P = pressureField(aa, z0, phases, xv, yv);
+
+    @series begin
+        linetype := :heatmap;
+        xv, yv, P
+    end
+
+
+    # If a list of agents is provided, plot dots at the places of the agents
+    if length(agents) != 0
+        agents_clrs = distinguishable_colors(length(agents), lchoices = range(40, stop=80, length=15), cchoices = range(30, stop=80, length=15))
+        for (k, agent) in enumerate(agents)
+            @series begin
+                markershape := :circle;
+                markersize := 4;
+                markercolor := agents_clrs[k];
+                # label := agent.name;
+                # legendfontsize := 4;
+                label := "";
+                linealpha := 0;
+                linetype := :path;
+
+                markerstrokecolor := :black;
+                markerstrokewidth := 1;
+
+                xc, yc, _ = agent.pos;
+                [xc], [yc]
+            end
+        end
+    end
+end
+
+end
